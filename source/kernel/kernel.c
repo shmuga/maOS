ffd4ca026a4a3f864204a942be306a181e007f88
Mark Orel <mail.ormark@gmail.com>
2013-12-19 05:30:22 +0200

    working kernel with drawing :D

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -1,26 +1,49 @@
- #include <multiboot.h>
+#include "multiboot.h"
 
-typedef struct multiboot_memory_map {
-	unsigned int size;
-	unsigned int base_addr_low,base_addr_high;
-// You can also use: unsigned long long int base_addr; if supported.
-	unsigned int length_low,length_high;
-// You can also use: unsigned long long int length; if supported.
-	unsigned int type;
-} multiboot_memory_map_t;
+#define TERM_WIDTH 80
+#define TERM_HEIGHT 24
 
-void write_string( int colour, const char *string )
+
+void print_char( int line, int pos, int fg_color ,int bg_color , char symbol)
 {
-    volatile char *video = (volatile char*)0xB8000;
-    while( *string != 0 )
-    {
-        *video++ = *string++;
-        *video++ = colour;
-    }
+	int letter = (bg_color*16*16*16+fg_color*16*16 + (int)symbol);
+	*((int*)(753664 + 160 * (line-1) + 2*(pos-1))) = letter;
+}
+
+void print_line(int line, int pos, int fg_color ,int bg_color , char *symbol){
+	int i=0;
+	while(symbol[i] != 0)
+	{
+		print_char(line,pos+i,fg_color,bg_color,symbol[i]);		
+		i++;
+	}
+		
+}
+
+void clear_terminal(int color){
+	for (int i=1;i<26;i++){
+		for (int j = 1; j <81; j++)
+		{
+			print_char(i,j,color,color,'a');
+		}		
+	}
 }
 
 void main(unsigned long magic, unsigned long addr)
 {
-	write_string(1,"hello");
+	
+	clear_terminal(0);
+	// *((int*)0xb8000)=1792 + 65+a;
+	// *((int*)0xb8000)=0x07690748 + a;
+	// *((int*)0xb80a0)=0x07690748;	
+	// print_char(2,45,1,15,'a');
+	// char i='a';
+	char hello[] = "hello";
+	char world[] = "world!";
+
+	// char j='a';
+	print_line(12,36,0,2,hello);
+	print_line(12,42,0,2,world);
+
 	for(;;);
 }
