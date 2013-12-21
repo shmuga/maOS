04dbe6b006993c890354a5ae3077f66ef7757884
Axel_Green <axelgreenkp@gmail.com>
2013-12-21 14:47:03 +0200

    work exeptions

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -3,14 +3,14 @@
 #define TERM_WIDTH 80
 #define TERM_HEIGHT 24
 
-
 void print_char( int line, int pos, int fg_color ,int bg_color , char symbol)
 {
 	int letter = (bg_color*16*16*16+fg_color*16*16 + (int)symbol);
 	*((int*)(753664 + 160 * (line-1) + 2*(pos-1))) = letter;
 }
 
-void print_line(int line, int pos, int fg_color ,int bg_color , char *symbol){
+void print_line(int line, int pos, int fg_color ,int bg_color , char *symbol)
+{
 	int i=0;
 	while(symbol[i] != 0)
 	{
@@ -20,7 +20,8 @@ void print_line(int line, int pos, int fg_color ,int bg_color , char *symbol){
 		
 }
 
-void clear_terminal(int color){
+void clear_terminal(int color)
+{
 	for (int i=1;i<26;i++){
 		for (int j = 1; j <81; j++)
 		{
@@ -29,21 +30,399 @@ void clear_terminal(int color){
 	}
 }
 
+
+
+/////////////////////////////////////////////////////////////////
+//for interruption table - IDT - The Interrupt Descriptor Table//
+/////////////////////////////////////////////////////////////////
+struct idt_entry
+{
+    unsigned short base_lo;
+    unsigned short sel;        
+    unsigned char always0;     
+    unsigned char flags;       
+    unsigned short base_hi;
+} __attribute__((packed));
+
+struct idt_ptr
+{
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+extern void idt_load();
+
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
+{
+    idt[num].base_lo = (base & 0xFFFF);
+    idt[num].base_hi = (base >> 16) & 0xFFFF;
+    idt[num].sel = sel;
+    idt[num].always0 = 0;
+    idt[num].flags = flags;
+}
+
+void *memset(void *dest, char val, int count)
+{
+    char *temp = (char *)dest;
+    for( ; count != 0; count--) *temp++ = val;
+    return dest;
+}
+
+void idt_install()
+{
+    idtp.limit = (sizeof (struct idt_entry) * 256) - 1;
+    idtp.base = (int)&idt;
+    memset(&idt, 0, sizeof(struct idt_entry) * 256);
+    idt_load();
+}
+
+
+/////////////////////////////////////////////////////
+//for exeptions - ISRs - Interrupt Service Routines//
+/////////////////////////////////////////////////////
+struct regs
+{
+    unsigned int gs, fs, es, ds;      
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;   
+    unsigned int eip, cs, eflags, useresp, ss;
+};
+
+extern void isr0();
+extern void isr1();
+extern void isr2();
+extern void isr3();
+extern void isr4();
+extern void isr5();
+extern void isr6();
+extern void isr7();
+extern void isr8();
+extern void isr9();
+extern void isr10();
+extern void isr11();
+extern void isr12();
+extern void isr13();
+extern void isr14();
+extern void isr15();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
+extern void isr24();
+extern void isr25();
+extern void isr26();
+extern void isr27();
+extern void isr28();
+extern void isr29();
+extern void isr30();
+extern void isr31();
+
+void isrs_install()
+{
+    idt_set_gate(0, (unsigned)isr0, 0x08, 0x8E);
+    idt_set_gate(1, (unsigned)isr1, 0x08, 0x8E);
+    idt_set_gate(2, (unsigned)isr2, 0x08, 0x8E);
+    idt_set_gate(3, (unsigned)isr3, 0x08, 0x8E);
+    idt_set_gate(4, (unsigned)isr4, 0x08, 0x8E);
+    idt_set_gate(5, (unsigned)isr5, 0x08, 0x8E);
+    idt_set_gate(6, (unsigned)isr6, 0x08, 0x8E);
+    idt_set_gate(7, (unsigned)isr7, 0x08, 0x8E);
+    idt_set_gate(8, (unsigned)isr8, 0x08, 0x8E);
+    idt_set_gate(9, (unsigned)isr9, 0x08, 0x8E);
+    idt_set_gate(10, (unsigned)isr10, 0x08, 0x8E);
+    idt_set_gate(11, (unsigned)isr11, 0x08, 0x8E);
+    idt_set_gate(12, (unsigned)isr12, 0x08, 0x8E);
+    idt_set_gate(13, (unsigned)isr13, 0x08, 0x8E);
+    idt_set_gate(14, (unsigned)isr14, 0x08, 0x8E);
+    idt_set_gate(15, (unsigned)isr15, 0x08, 0x8E);
+    idt_set_gate(16, (unsigned)isr16, 0x08, 0x8E);
+    idt_set_gate(17, (unsigned)isr17, 0x08, 0x8E);
+    idt_set_gate(18, (unsigned)isr18, 0x08, 0x8E);
+    idt_set_gate(19, (unsigned)isr19, 0x08, 0x8E);
+    idt_set_gate(20, (unsigned)isr20, 0x08, 0x8E);
+    idt_set_gate(21, (unsigned)isr21, 0x08, 0x8E);
+    idt_set_gate(22, (unsigned)isr22, 0x08, 0x8E);
+    idt_set_gate(23, (unsigned)isr23, 0x08, 0x8E);
+    idt_set_gate(24, (unsigned)isr24, 0x08, 0x8E);
+    idt_set_gate(25, (unsigned)isr25, 0x08, 0x8E);
+    idt_set_gate(26, (unsigned)isr26, 0x08, 0x8E);
+    idt_set_gate(27, (unsigned)isr27, 0x08, 0x8E);
+    idt_set_gate(28, (unsigned)isr28, 0x08, 0x8E);
+    idt_set_gate(29, (unsigned)isr29, 0x08, 0x8E);
+    idt_set_gate(30, (unsigned)isr30, 0x08, 0x8E);
+    idt_set_gate(31, (unsigned)isr31, 0x08, 0x8E);
+}
+
+char *exception_messages[] =
+{
+    "Division By Zero",
+    "Debug",
+    "Non Maskable Interrupt",
+    "Breakpoint",
+    "Into Detected Overflow",
+    "Out of Bounds",
+    "Invalid Opcode",
+    "No Coprocessor",
+    "Double Fault",
+    "Coprocessor Segment Overrun",
+    "Bad TSS",
+    "Segment Not Present",
+    "Stack Fault",
+    "General Protection Fault",
+    "Page Fault",
+    "Unknown Interrupt",
+    "Coprocessor Fault",
+    "Alignment Check",
+    "Machine Check",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved",
+    "Reserved"
+};
+
+void fault_handler(struct regs *r)
+{
+    if (r->int_no < 32)
+    {
+    	// exception_messages[r->int_no]
+        print_line(12,36,0,2,exception_messages[r->int_no]);
+        for (;;);
+    }
+}
+
+
+//////////////////////////////////////////////
+//for devices - IRQs - Interrupt Requests or// 
+//////////////////////////////////////////////
+extern void irq0();
+extern void irq1();
+extern void irq2();
+extern void irq3();
+extern void irq4();
+extern void irq5();
+extern void irq6();
+extern void irq7();
+extern void irq8();
+extern void irq9();
+extern void irq10();
+extern void irq11();
+extern void irq12();
+extern void irq13();
+extern void irq14();
+extern void irq15();
+
+int irq_routines[16] =
+{
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0
+};
+
+// void irq_install_handler(int irq, void (*handler)(struct regs *r))
+// {
+//     irq_routines[irq] = &handler;
+// }
+
+void irq_install_handler(int irq, int (*handler)(struct regs *r))
+{
+    irq_routines[irq] = (int)handler;
+}
+
+void irq_uninstall_handler(int irq)
+{
+    irq_routines[irq] = 0;
+}
+
+void outportb (unsigned short _port, unsigned char _data)
+{
+    __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
+}
+
+void irq_remap(void)
+{
+    outportb(0x20, 0x11);
+    outportb(0xA0, 0x11);
+    outportb(0x21, 0x20);
+    outportb(0xA1, 0x28);
+    outportb(0x21, 0x04);
+    outportb(0xA1, 0x02);
+    outportb(0x21, 0x01);
+    outportb(0xA1, 0x01);
+    outportb(0x21, 0x0);
+    outportb(0xA1, 0x0);
+}
+
+void irq_install()
+{
+    irq_remap();
+
+    idt_set_gate(32, (unsigned)irq0, 0x08, 0x8E);
+    idt_set_gate(33, (unsigned)irq1, 0x08, 0x8E);
+    idt_set_gate(34, (unsigned)irq2, 0x08, 0x8E);
+    idt_set_gate(35, (unsigned)irq3, 0x08, 0x8E);
+    idt_set_gate(36, (unsigned)irq4, 0x08, 0x8E);
+    idt_set_gate(37, (unsigned)irq5, 0x08, 0x8E);
+    idt_set_gate(38, (unsigned)irq6, 0x08, 0x8E);
+    idt_set_gate(39, (unsigned)irq7, 0x08, 0x8E);
+
+    idt_set_gate(40, (unsigned)irq8, 0x08, 0x8E);
+    idt_set_gate(41, (unsigned)irq9, 0x08, 0x8E);
+    idt_set_gate(42, (unsigned)irq10, 0x08, 0x8E);
+    idt_set_gate(43, (unsigned)irq11, 0x08, 0x8E);
+    idt_set_gate(44, (unsigned)irq12, 0x08, 0x8E);
+    idt_set_gate(45, (unsigned)irq13, 0x08, 0x8E);
+    idt_set_gate(46, (unsigned)irq14, 0x08, 0x8E);
+    idt_set_gate(47, (unsigned)irq15, 0x08, 0x8E);
+}
+
+void irq_handler(struct regs *r)
+{
+    int (*handler)(struct regs *r);
+    // int (handler)(struct regs *r);
+    handler = (irq_routines[r->int_no - 32]);
+    if (handler)
+    {
+        handler(r);
+    }
+    if (r->int_no >= 40)
+    {
+        outportb(0xA0, 0x20);
+    }
+    outportb(0x20, 0x20);
+}
+
+//////////////////////////////////////////////
+//for keyboard//
+//////////////////////////////////////////////
+ char kbdus[128] =
+{
+    0,  27, '1', '2', '3', '4', '5', '6', '7', '8',	/* 9 */
+  '9', '0', '-', '=', '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'q', 'w', 'e', 'r',	/* 19 */
+  't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',	/* 39 */
+ '\'', '`',   0,		/* Left shift */
+ '\\', 'z', 'x', 'c', 'v', 'b', 'n',			/* 49 */
+  'm', ',', '.', '/',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0,
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */
+};		
+
+unsigned char inportb (unsigned short _port)
+{
+    unsigned char rv;
+    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
+    return rv;
+}
+
+int keyboard_handler(struct regs *r)
+{
+	// clear_terminal(0);
+ //    char hello[] = "hello world!";
+	// print_line(12,36,0,2,hello);
+
+    unsigned char scancode;
+
+    /* Read from the keyboard's data buffer */
+    scancode = inportb(0x60);
+
+    /* If the top bit of the byte we read from the keyboard is
+    *  set, that means that a key has just been released */
+    if (scancode & 0x80)
+    {
+    	clear_terminal(0);
+    	char hello[] = "hello world!";
+    	print_line(12,36,0,2,&kbdus[scancode]);
+        /* You can use this one to see if the user released the
+        *  shift, alt, or control keys... */
+    }
+    else
+    {
+        /* Here, a key was just pressed. Please note that if you
+        *  hold a key down, you will get repeated key press
+        *  interrupts. */
+
+        /* Just to show you how this works, we simply translate
+        *  the keyboard scancode into an ASCII value, and then
+        *  display it to the screen. You can get creative and
+        *  use some flags to see if a shift is pressed and use a
+        *  different layout, or you can add another 128 entries
+        *  to the above layout to correspond to 'shift' being
+        *  held. If shift is held using the larger lookup table,
+        *  you would add 128 to the scancode when you look for it */
+        // putch(kbdus[scancode]);
+        // clear_terminal(0);
+        // char hello[] = "hello world!";
+        // print_line(12,36,0,2,hello);
+        print_line(12,36,0,2,&kbdus[scancode]);
+    }
+}
+
+void keyboard_install()
+{
+    // void test(void *r);
+    // irq_install_handler(1, (void *) &test);
+    irq_install_handler(1, &keyboard_handler);
+}
+
+////////////////////////////////////////////////
+
 void main(unsigned long magic, unsigned long addr)
 {
+
+	idt_install();
+	isrs_install();
+	irq_install();	
+	keyboard_install();
+    __asm__ __volatile__ ("sti");
+	// clear_terminal(0);
+	// char hello[] = "hello world!";
+
+	// int y = 0;
+	// int x = 1/y;
+
+	// print_line(12,36,0,2,hello);
 	
-	clear_terminal(0);
-	// *((int*)0xb8000)=1792 + 65+a;
-	// *((int*)0xb8000)=0x07690748 + a;
-	// *((int*)0xb80a0)=0x07690748;	
-	// print_char(2,45,1,15,'a');
-	// char i='a';
-	char hello[] = "hello world!";
-	// char world[] = "world";
-
-	// char j='a';
-	print_line(12,36,0,2,hello);
-	// print_line(12,42,0,2,world);
-
 	for(;;);
 }
