67e040102cc94d35169635c345c455c488500d9f
Mark Orel <mail.ormark@gmail.com>
2013-12-21 15:00:32 +0200

    working idt mark

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 export C_INCLUDE_PATH=$C_INCLUDE_PATH:source/headers
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libXpm.so.4
 
-gcc_flags="-pedantic -pipe -Werror -std=c99 -march=i386 -m32 -fno-asynchronous-unwind-tables -fno-stack-protector -nostdinc -ffreestanding"
+gcc_flags="-pedantic -pipe -Werror -std=c99 -march=i386 -m32 -fno-asynchronous-unwind-tables  -fno-builtin -fno-stack-protector  -nostdlib -nostdinc -ffreestanding -nostartfiles -nodefaultlibs -Wall"
 
 nasm -f elf32 source/bootloader/loader.s -o build/bootloader/loader.o
+nasm -f elf32 source/bootloader/idt.s -o build/bootloader/idt.o
 gcc $gcc_flags -c source/kernel/*.c  -o build/kernel/kernel.o
 gcc $gcc_flags -c source/libs/*.c 
 mv *.o build/libs/
@@ -16,7 +17,7 @@ cp grub/menu.lst iso/boot/grub/
 genisoimage -R                              \
             -b boot/grub/stage2_eltorito    \
             -no-emul-boot                   \
-            -boot-load-size 400               \
+            -boot-load-size 400             \
             -A os                           \
             -input-charset utf8             \
             -quiet                          \
