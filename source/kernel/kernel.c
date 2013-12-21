1903c52382eeed1db42e0c77a52c22bde264bcb0
Mark Orel <mail.ormark@gmail.com>
2013-12-22 01:34:47 +0200

    xz

--- source/kernel/kernel.c
+++ source/kernel/kernel.c
@@ -3,28 +3,13 @@
 
 void main(unsigned long magic, unsigned long addr)
 {	
-<<<<<<< HEAD
-	init_video();	
-	cls();
-	puts((unsigned char*)"This is the beggining of the new ERA!");
-	gdt_install();
-    idt_install();
-    isrs_install();
-    irq_install();
-    // init_video();    
-    // timer_install();
-    keyboard_install();
-
-    __asm__ __volatile__ ("sti");
-=======
     gdt_install();
     idt_install();
     isrs_install();
     irq_install();
     init_video();
-    timer_install();
+    // timer_install();
     keyboard_install();
->>>>>>> ce29ddb43b5bcb10d2ed3d5df0a594415c3c6bd7
 
     __asm__ __volatile__ ("sti");
 	cls();
